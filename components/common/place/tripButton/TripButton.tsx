8d265a31c193a4a09e3cbef8d3fd099c4a790f0e
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-09 22:39:14 +0300

    fixed styles for button

--- components/common/place/tripButton/TripButton.tsx
+++ components/common/place/tripButton/TripButton.tsx
@@ -5,7 +5,6 @@ const TripButton = () => {
         <button className={styles.button}>
             <div className={styles.button__text}>Book a trip</div>
             <span className='material-icons-outlined'>chevron_right</span>
-
         </button>
     )
 }

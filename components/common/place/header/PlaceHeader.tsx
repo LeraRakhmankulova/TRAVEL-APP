1264560d3ced19994ec15f11148b3f0def1b2345
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-09 22:40:03 +0300

    styles

--- components/common/place/header/PlaceHeader.tsx
+++ components/common/place/header/PlaceHeader.tsx
@@ -1,14 +1,21 @@
 import styles from './index.module.sass'
+import {useState} from "react";
+import Favorites from "../favorites/Favorites";
+import Link from "next/link";
 
 const PlaceHeader = () => {
+    const [visible, setVisible] = useState(false)
     return (
         <div className={styles.header}>
-            <button>
-                <span className="material-icons-outlined">arrow_back_ios_new</span>
-            </button>
-            <button>
-                <span className="material-icons-outlined">bookmark</span>
-            </button>
+            <div className={styles.header__wrapper}>
+                <Link href='/'>
+                    <span className="material-icons-outlined">arrow_back_ios_new</span>
+                </Link>
+                <button onClick={() => setVisible(!visible)}>
+                    <span className="material-icons-outlined">{visible ? "bookmark" : "bookmark_border"}</span>
+                </button>
+            </div>
+            {visible && <Favorites/>}
         </div>
     )
 }

f43e7e4644cd58b9e5ba7a0c70e413bad7118753
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-10 21:06:38 +0300

    progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/assets/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import NextProgressBar from 'nextjs-progressbar'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <NextProgressBar
+                color="#eb601e"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={3}
+                showOnShallow={false}
+                nonce="my-nonce"
+                options={{easing: 'ease', speed: 500}}
+            />
+            <Component {...pageProps} />
+        </>
+    )
 }

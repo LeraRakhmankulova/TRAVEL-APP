35b694d41a19393ea863dae61f5fe2e61407b8fc
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-11-15 00:06:01 +0300

    weclome page done

--- app/components/common/welcome/WelcomePage.tsx
+++ app/components/common/welcome/WelcomePage.tsx
@@ -1,24 +1,55 @@
 import styles from './index.module.sass'
 import map from '@/assets/images/welcomeMap.svg';
-import place from '@/assets/images/placeItem1.svg';
+import fPlace from '@/assets/images/placeItem1.svg';
+import sPlace from '@/assets/images/placeItem2.svg';
+import tPlace from '@/assets/images/placeItem3.svg';
 import {motion, useMotionValue, useTransform} from "framer-motion"
+import Link from "next/link";
+
+const placeImgPath = [
+    {
+        placePath: fPlace.src,
+        mLeft: 100,
+        mTop: 350,
+        duration: 0.7
+    },
+    {
+        placePath: sPlace.src,
+        mLeft: 200,
+        mTop: 300,
+        duration: 1.5
+    },
+    {
+        placePath: tPlace.src,
+        mLeft: 650,
+        mTop: -680,
+        duration: 2.5
+    }
+]
+
 
 const WelcomePage = () => {
     return (
         <div className={styles.welcome}>
             <div style={{backgroundImage: `url("${map.src}")`}} className={styles.welcome__map}>
                 <div className={styles.welcome__items}>
-                    <motion.div
-                        initial={{opacity: 0, scale: 0.7}}
-                        animate={{opacity: 1, scale: 1}}
-                        transition={{duration: 0.65}}>
-                        <img src={place.src}/>
-                    </motion.div>
+                    {placeImgPath.map(item =>
+                        <motion.div
+                            initial={{opacity: 0, scale: 0.7}}
+                            animate={{opacity: 1, scale: 1}}
+                            transition={{duration: `${item.duration}`}}
+                            style={{margin: `${item.mTop}% 0 0 ${item.mLeft}%`}}>
+                            <img src={item.placePath} width={55}/>
+                        </motion.div>)
+                    }
                 </div>
             </div>
             <div className={styles.welcome__text}>
                 <h1>Free yourself</h1>
                 <h4>Explore amazing places around the globe.</h4>
+                <Link className={styles.welcome__button} href='/'>
+                    <button>Let's go!</button>
+                </Link>
             </div>
         </div>
     )
